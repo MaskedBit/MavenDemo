3e3d32a5f021a7914457b78be25e3029386e636a
Steve Boylan <sboylan@leeriversoftware.com>
2016-12-28 14:57:04 -0500

    First version of code

---
+++ maven-demo/src/main/java/edu/tutor/maven/App.java
@@ -0,0 +1,13 @@
+package edu.tutor.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Howdy, folks!" );
+    }
+}
